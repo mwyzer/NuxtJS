eb43e4c0edf65aada6176c35069dfc935ea5f6ce
mwyzer <muhammad.wyzer@gmail.com>
2023-01-03 00:22:11 +0700

    reusable components

---
+++ layouts/products.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merchant</NuxtLink>
+            </nav>
+        </header>
+
+        <!-- output the page content -->
+        <siv class="container mx-auto p-4 flex justify-between border-t-2">
+            <slot />
+        </siv>
+
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <div>
+                <ul class="flex gap-4">
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+.ul.no-bullets {
+    list-style-type: square;
+    padding: 0;
+    margin: 0;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam in debitis cum eaque, illo, eius saepe
+            incidunt a quisquam dolore nisi nulla ipsum necessitatibus vitae alias minima, et vel. Consequatur?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam in debitis cum eaque, illo, eius saepe
+            incidunt a quisquam dolore nisi nulla ipsum necessitatibus vitae alias minima, et vel. Consequatur?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+     margin-bottom: 20px;
+     font-size: 36px;
+ }
+
+ p {
+     margin: 20px 0;
+ }
+</style>
